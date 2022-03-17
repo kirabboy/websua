3c57661e34bb50bf92c1d2a5a92fff6521ccd6f9
nguyenphanquocthinh <44148967+nguyenphanquocthinh@users.noreply.github.com>
2022-03-17 14:32:20 +0700

    14:30

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,9 +8,6 @@
     <!-- Custom fonts for this template-->
     <link href="{{ asset('/public/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="url-home" content="{{ URL::to('/') }}" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/boostrap/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
     <!-- <link href="{{ asset('/public/vendor/css/sb-admin-2.min.css') }}" rel="stylesheet"> -->
